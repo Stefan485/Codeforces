168e51b7913700a54c79f159c13ff99faffc6f37
Stefan485 <stefan.stefanobradovic@gmail.com>
2022-08-05 11:59:32 +0200

    Added: Drinks

---
+++ codeforces/Drinks/src/Drinks.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Drinks {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int drinks = input.nextInt();
+        float sum = 0;
+        for(int i = 0; i < drinks; i ++){
+            sum += input.nextInt();
+        }
+        System.out.println(sum/drinks);
+        input.close();
+    }
+}
