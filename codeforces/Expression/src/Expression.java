3fc3597ae69982e3e7f8d17ab7ab5979f72a75a9
Stefan485 <stefan.stefanobradovic@gmail.com>
2022-08-10 17:54:41 +0200

    Added: Expression

---
+++ codeforces/Expression/src/Expression.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Expression {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int res1 = (a + b) * c;
+        int res2 = a * (b + c);
+        int res3 = a * b * c;
+        int res4 = a + b + c;
+        int max = Math.max(res1, res2);
+        max = Math.max(max, res3);
+        max = Math.max(max, res4);
+        System.out.println(max);
+
+        input.close();
+    }
+}
