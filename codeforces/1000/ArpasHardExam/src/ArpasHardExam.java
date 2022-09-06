157b91671a12c1157cce0a80a9e3b8fb1c65200a
Stefan485 <stefan.stefanobradovic@gmail.com>
2022-09-06 20:57:12 +0200

    Added: Arpas hard exam

---
+++ codeforces/1000/ArpasHardExam/src/ArpasHardExam.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArpasHardExam {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = {8, 4, 2, 6};
+        if(n == 0){
+            System.out.println(1);
+        }
+        else if(n % 4 == 0){
+            System.out.println(arr[3]);
+        }
+        else{
+            System.out.println(arr[n % 4 - 1]);
+        }
+
+
+
+
+        input.close();
+    }
+}
