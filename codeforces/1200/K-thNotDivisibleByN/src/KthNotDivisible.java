de1c19b60940b1ef3a702d69715f743840232484
Stefan485 <stefan.stefanobradovic@gmail.com>
2022-09-08 22:16:50 +0200

    Added: K-th not divisible by n

---
+++ codeforces/1200/K-thNotDivisibleByN/src/KthNotDivisible.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class KthNotDivisible {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int testCases = input.nextInt();
+        int i = 0;
+                
+        while(i < testCases){
+            int n = input.nextInt();
+            int k = input.nextInt();
+
+            int number = k / (n - 1) * n;
+            if(k % (n - 1) == 0 ){
+                number--;
+            }
+            else{
+                number += k % (n - 1);
+            }
+            System.out.println(number);
+            i++;
+        }
+
+        input.close();
+    }
+}
