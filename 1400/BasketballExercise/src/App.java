4f3d23a477774868457584b14537ea8abd11524a
Stefan485 <stefan.stefanobadovic@gmail.com>
2023-10-27 00:24:36 +0200

    Basketball Exercise

---
+++ 1400/BasketballExercise/src/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+        int numberOfPlayers = reader.nextInt();
+        long[] arr1 = new long[numberOfPlayers + 1];
+        long[] arr2 = new long[numberOfPlayers + 1];
+ 
+        for(int i = 1; i <= numberOfPlayers; i++) {
+            arr1[i] = reader.nextLong();
+        }
+
+        for(int i = 1; i <= numberOfPlayers; i++) {
+            arr2[i] = reader.nextLong();
+        }
+
+        long[][] dp = new long[numberOfPlayers + 1][2];
+        
+
+        for(int i = 1; i <= numberOfPlayers; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1] + arr1[i], dp[i - 1][0]);
+            dp[i][1] = Math.max(dp[i - 1][0] + arr2[i], dp[i - 1][1]);
+        }
+
+        System.out.println(Math.max(dp[numberOfPlayers][0], dp[numberOfPlayers][1]));
+        reader.close();
+    }
+}
